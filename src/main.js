e9c8e532f92e6e27cf9ad1b27cc5cd36f491957c
luvsseul <150340509+luvsseul@users.noreply.github.com>
2023-11-18 18:03:40 +0900

    JS-home

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 'use strict';
 
 const header = document.querySelector('.header');
-const headerHeight = header.clientHeight;
+const headerHeight = header.offsetHeight;
 
+//헤더영역 스크롤 되며 다크닝하기
 document.addEventListener('scroll', () => {
-    if (headerHeight < window.scrollY) {
+    if (window.scrollY > headerHeight) {
         header.classList.add('header--dark');
     }
     else {
         header.classList.remove('header--dark');
     }
+});
+
+//홈영역 스크롤되며 투명도 키우기
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
