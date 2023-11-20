72d359f54b51bb8281ad0d757e6170063753ae0b
luvsseul <150340509+luvsseul@users.noreply.github.com>
2023-11-20 22:14:34 +0900

    JS-projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,35 @@
 'use strict';
+
+
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectBox = document.querySelector('.projects');
+categories.addEventListener('click', (e) => {
+    const targetCategory = e.target.dataset.category;
+    if (targetCategory == null) {
+        return;
+    }
+    handleActiveSelection(e.target);
+    filterProjects(targetCategory);
+});
+
+function handleActiveSelection(target) {
+    const active = document.querySelector('.category--selected');
+    active.classList.remove('category--selected');
+    target.classList.add('category--selected');
+};
+
+function filterProjects(targetCategory) {
+    projectBox.classList.add('anim-out');
+    projects.forEach(e => {
+        if (targetCategory === 'all' || targetCategory === e.dataset.type) {
+            e.style.display = "block";
+        }
+        else {
+            e.style.display = "none";
+        }
+    });
+    setTimeout(() => {
+        projectBox.classList.remove('anim-out');
+    }, 250)
+};
